b7c67fff76ea37452a58c92382d10df2a95bf76f
Syahir AR <38739231+syahir98-AR@users.noreply.github.com>
2021-05-25 02:05:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,22 +12,22 @@
 #     app.run(host='127.0.0.1')
 
 import urllib.request
+import io
 import json
 import os
 import ssl
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-# def runApp():
-#     return 'Hello World'
 
 def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
     if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+# this line is needed if you use self-signed certificate in your scoring service.
+allowSelfSignedHttps(True)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -35,9 +35,8 @@ cors = CORS(app)
 input6 = "2"
 input7 = "3"
 
-@app.route('/', methods = ['GET'])
 
-# // Request data goes here
+@app.route('/', methods=['GET', 'POST'])
 def requestModel():
 
     global input6
@@ -49,7 +48,7 @@ def requestModel():
         input1 = request_data['input6']
         input2 = request_data['input7']
         return "Hello World!"
-        
+
     elif(request.method == "GET"):
         data = {
             "Inputs": {
@@ -76,8 +75,10 @@ def requestModel():
         body = str.encode(json.dumps(data))
 
         url = 'http://20.195.98.200:80/api/v1/service/model-decisiontree/score'
-        api_key = 'fiUUUwL28J9AxzAbxB8gLmg3n4bcGEkJ' # Replace this with the API key for the web service
-        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+        # Replace this with the API key for the web service
+        api_key = 'fiUUUwL28J9AxzAbxB8gLmg3n4bcGEkJ'
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': ('Bearer ' + api_key)}
 
         req = urllib.request.Request(url, body, headers)
 
@@ -85,9 +86,12 @@ def requestModel():
             response = urllib.request.urlopen(req)
 
             result = response.read()
-            print(result)
-            print(request.method)
-            return result
+
+            final_result = result.replace(b"'", b"'")
+            my_json = json.load(io.BytesIO(final_result))
+            print(my_json)
+            return my_json
+
         except urllib.error.HTTPError as error:
             print("The request failed with status code: " + str(error.code))
 
@@ -95,6 +99,8 @@ def requestModel():
             print(error.info())
             print(json.loads(error.read().decode("utf8", 'ignore')))
 
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1')
+    app.run(host='127.0.0.1', port=8000)
+
 
