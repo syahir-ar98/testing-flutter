224919def50ece04a80893312e9296389a971d0e
Syahir AR <38739231+syahir98-AR@users.noreply.github.com>
2021-05-25 02:53:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,21 +32,45 @@ allowSelfSignedHttps(True)
 app = Flask(__name__)
 cors = CORS(app)
 
-input6 = "2"
-input7 = "3"
+input1 = "true"
+input2 = "true"
+input3 = "true"
+input4 = "true"
+input5 = "true"
+input6 = "0"
+input7 = "0"
+input8 = "0"
+input9 = "0"
+input10 = "0"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def requestModel():
 
+    global input1
+    global input2
+    global input3
+    global input4
+    global input5
     global input6
     global input7
+    global input8
+    global input9
+    global input10
 
     if(request.method == "POST"):
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
-        input1 = request_data['input6']
-        input2 = request_data['input7']
+        input1 = request_data['input1']
+        input2 = request_data['input2']
+        input3 = request_data['input3']
+        input4 = request_data['input4']
+        input5 = request_data['input5']
+        input6 = request_data['input6']
+        input7 = request_data['input7']
+        input8 = request_data['input8']
+        input9 = request_data['input9']
+        input10 = request_data['input10']
         return "Hello World!"
 
     elif(request.method == "GET"):
@@ -55,16 +79,16 @@ def requestModel():
                 "WebServiceInput0":
                 [
                     {
-                        'Quiz 1_Q1 Score': "false",
-                        'Quiz 1_Q2 Score': "false",
-                        'Quiz 1_Q3 Score': "false",
-                        'Quiz 1_Q4 Score': "true",
-                        'Quiz 1_Q5 Score': "true",
+                        'Quiz 1_Q1 Score': input1,
+                        'Quiz 1_Q2 Score': input2,
+                        'Quiz 1_Q3 Score': input3,
+                        'Quiz 1_Q4 Score': input4,
+                        'Quiz 1_Q5 Score': input5,
                         'Quiz 1_Q1 Time': input6,
                         'Quiz 1_Q2 Time': input7,
-                        'Quiz 1_Q3 Time': "2",
-                        'Quiz 1_Q4 Time': "1",
-                        'Quiz 1_Q5 Time': "2",
+                        'Quiz 1_Q3 Time': input8,
+                        'Quiz 1_Q4 Time': input9,
+                        'Quiz 1_Q5 Time': input10,
                     },
                 ],
             },
@@ -101,6 +125,7 @@ def requestModel():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000)
+    app.run(debug=True, host='127.0.0.1', port=8000)
+
 
 
