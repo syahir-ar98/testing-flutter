9a5e3c21df6be543240ed05a19b85059b2a22502
Syahir AR <38739231+syahir98-AR@users.noreply.github.com>
2021-05-25 00:55:26 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/', methods = ['GET'])
+def runApp():
+    return 'Hello World'
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1')
